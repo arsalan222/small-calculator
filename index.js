c73bad558ceeceeea878038083fa63845aff6c4f
Muhammad Arsalan <muhammadarsalan066@gmail.com>
2021-08-13 17:04:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+// var animal = ["hen","elephant","deer","cobra"]
+// console.log(animal)
+
+//  animal[1] = "raju";
+//  console.log(animal);
+
+//  animal.pop();
+//  console.log(animal);
+
+//  animal.push("ajjj")
+//  console.log(animal)
+
+//  animal.splice(1,2,"hores","goat")
+//  console.log(animal)
+
+
+//  for (var i=1; i<=10 ; i++)
+//  {
+//      console.log("2" + "x" + i + "=" + 2*i)
+
+//  }
+
+//    var student = ["arsalan","Noman","Humza","raja"]
+
+
+//  for (var i=0 ; i<student.length;i++)
+//  {
+//      console.log(student[i])
+//  }
+
+// var student = ["karachi","lahore"]
+
+// for (var i=0 ; i<student.length;i++)
+// {
+//     if(student[i]=== "karachi")
+//     {
+//         console.log("wao nice")
+//     }
+//     if(student[i]!= "karachi")
+//     {
+//         console.log("no i am not")
+//     }
+// }
+
+
+// firstname = ["arsalan","Farhan","raju"]
+// lastname = ["sabir", "safdar"]
+//  for (var a = 0; a<firstname.length ; a++)
+// {
+//     for(var b = 0; b<lastname.length ; b++)
+//     {
+//         console.log("i"+ "="+ firstname[a] + " " + "z"+ "=" + lastname[b] )
+//     }
+
+// }
+
+   
+// var word = prompt("enter the word")
+
+// for (var a=word.length -1; a>=0 ; a--)
+// {
+//   console.log(word[a])
+// }
+
+// calculator
+
+
+ var value1= +prompt("enter the value 1")
+ var value2 = +prompt("enter the value 2")
+ var sign  = prompt("enter the sign ")
+
+if(sign==="+")
+{
+  console.log(value1+value2)
+}
+else if(sign==="/")
+{
+  console.log(value1/value2)
+}
+else if (sign==="*")
+{
+  console.log(value1*vlaue2)
+
+}
+
+else
+
+{
+  console.log("error found")
+}
+
+
+
+// var arr = [45,63,84,1,52,22]
+// found = false;
+// var input = parseInt(prompt("any value you enter "));
+// console.log("user input" + input)
+
+// for (var i=0; i < arr.length; i++) {
+//     console.log(arr[i])
+//     if(arr[i] === input)
+//  {
+
+//     console.log("yes you are right=" + input )
+//     found = true;
+//     break;
+//  }
+//  if (found ===false)
+//  console.log("their is no way")
+
+// } 
